917dea14ccfe93697a07c51af39aa1c5b7c97be1
suryadeepyadav <95708869+suryadeepyadav@users.noreply.github.com>
2021-12-09 23:54:13 +0530

    my snake game website
    in this website we made a snake game

---
+++ snake game/js/index.js
@@ -0,0 +1,152 @@
+//game constants and variables
+let inputDir = {x:0,y:0};
+const foodsound = new Audio('food.mp3');
+const gameoversound = new Audio('gameover.mp3');
+const movesound = new Audio('move.mp3');
+const musicsound = new Audio('music.mp3');
+let speed = 5;
+let score = 0;
+let lastPaintTime = 0;
+let snakeArr = [
+    {x:13,y:15}
+];
+
+food = {x:6,y:7}; 
+
+
+//game functions
+function main(ctime) {
+    window.requestAnimationFrame(main);
+    //console.log(ctime)
+    if((ctime - lastPaintTime)/1000 <1/speed){
+        return;
+    }
+    lastPaintTime = ctime;
+    gameEngine();
+   
+}
+function isCollide(snake){
+    //if you bump into yourself
+    for (let i = 1; i < snakeArr.length; i++) {
+        if(snake[i].x===snake[0].x && snake[i].y===snake[0].y){
+            return true;
+        }
+    }   
+     if(snake[0].x >= 18 || snake[0].x <=0 || snake[0].y >= 18 || snake[0].y <=0){
+            return true;
+    }      
+    
+}
+//if you bump into the wall 
+function gameEngine(){
+    //part1:updating the snake array and food 
+    if(isCollide(snakeArr)){
+        gameoversound.play();
+        musicsound.pause();
+        inputDir ={x:0,y:0};
+        alert("Game over press any key to play again");
+        snakeArr = [{x:13,y:15}];
+        //musicsound.play();
+        score = 0;
+    }
+
+    //if you have eten the food ,increament the score and regenerate th food
+    if(snakeArr[0].y===food.y && snakeArr[0].x ===food.x){
+        foodsound.play();
+        score +=1;
+        if(score>hiscoreval){
+            hiscoreval = score;
+            localStorage.setItem("hiscore",JSON.stringify(hiscoreval));
+            hiscoreBox.innerHTML = "Hiscore:" + hiscoreval;
+        }
+        scoreBox.innerHTML = "score:"+ score;
+        snakeArr.unshift({x: snakeArr[0].x + inputDir.x , y: snakeArr[0].y + inputDir.y});
+        let a = 2;
+        let b = 16;
+        food = {x: Math.round(a + (b-a)*Math.random()) , y: Math.round(a + (b-a)*Math.random())}
+    }
+
+    //moving the snake 
+    for (let i = snakeArr.length - 2 ; i>=0; i--) {
+        snakeArr[i+1] = {...snakeArr[i]};
+        
+    }
+
+    snakeArr[0].x += inputDir.x;
+    snakeArr[0].y += inputDir.y;
+
+    //part2:display the snake and food
+    //display the snake
+    board.innerHTML = "";
+    snakeArr.forEach((e, index)=>{
+        snakeElement = document.createElement('div');
+        snakeElement.style.gridRowStart = e.y;
+        snakeElement.style.gridColumnStart = e.x;
+        
+        if(index == 0){
+            snakeElement.classList.add('head');
+        }
+        else{
+            snakeElement.classList.add('snake');
+        }
+        board.appendChild(snakeElement); 
+    });
+    //display the food
+        foodElement = document.createElement('div');
+        foodElement.style.gridRowStart = food.y;
+        foodElement.style.gridColumnStart = food.x;
+        foodElement.classList.add('food')
+        board.appendChild(foodElement); 
+
+}
+
+
+
+        
+
+
+
+
+//main logic start from here 
+let hiscore = localStorage.getItem("hiscore");
+if(hiscore===null){
+    hiscoreval = 0;
+    localStorage.setItem("hiscore",JSON.stringify(hiscoreval))
+}
+else{
+    hiscoreval = JSON.parse(hiscore);
+    hiscoreBox.innerHTML = "Hiscore:" + hiscore;
+}
+window.requestAnimationFrame(main);
+window.addEventListener('keydown',e =>{
+    inputDir = {x:0,y:1}//start the game
+    movesound.play();
+    switch (e.key) {
+        case "ArrowUp":
+            console.log("ArrowUp");
+            inputDir.x = 0;
+            inputDir.y = -1;
+            break;
+
+        case "ArrowDown":
+            console.log("ArrowDown");
+            inputDir.x = 0;
+            inputDir.y = 1;
+            break;
+
+        case "ArrowLeft":
+            console.log("ArrowLeft");
+            inputDir.x = -1;
+            inputDir.y = 0;
+            break;
+
+        case "ArrowRight":
+            console.log("ArrowRight");
+            inputDir.x = 1;
+            inputDir.y = 0;
+            break;
+    
+        default:
+            break;
+    }
+});
